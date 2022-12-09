064a72d025d54e4c4c40da4d412119e09e20fbe3
Tucky28 <116147978+Tucky28@users.noreply.github.com>
2022-12-10 10:35:59 +1100

    Created pet class with variables for all pets

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import datetime
 from random import randrange
 import pickle
+
+# Created pet class with variables for all pets
+class pet(object):
+    age: 0
+    excitement_reduce = 3
+    excitement_max = 10
+    excitement_warning = 3
+    food_reduce = 2
+    food_max = 10
+    food_warning = 2
+    level: 0
+    sleep_reduce = 1
+    sleep_max = 10
+    vocab = [""]
+
+        
+
+
+
