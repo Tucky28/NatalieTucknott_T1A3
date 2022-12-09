e3ed3ee243a66241ca3cf1922dc3714358ed44a4
Tucky28 <116147978+Tucky28@users.noreply.github.com>
2022-12-10 10:39:58 +1100

    Created constructor for pet

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ class pet(object):
     sleep_max = 10
     vocab = [""]
 
+    # Created constructor for pet
+    def __init__(self, name, pet_type):
+        self.age = self.age
+        self.excitement = randrange(self.excitement_max)
+        self.food = randrange(self.food_max)
+        self.level = self.level
+        self.name = self.name
+        self.pet_type = self.pet_type
+        self.sleep = randrange(self.sleep_max)
+        self.vocab = self.vocab[:]
         
 
 
