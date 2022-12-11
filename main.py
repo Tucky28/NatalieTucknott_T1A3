df9a537c2736f30dd29a12adfff6411be20f3df9
Tucky28 <116147978+Tucky28@users.noreply.github.com>
2022-12-11 22:17:37 +1100

    Defined bedtime function for sleep

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ class Pet(object):
     pet_type = [""]
     age = 0
     excitement_reduce = 2
+    excitement_min = 0
     excitement_max = 10
     excitement_warning = 3
+    food_min = 0
     food_reduce = 2
     food_max = 10
     food_warning = 2
     level = 0
     sleep_reduce = 1
+    sleep_min = 0
     sleep_max = 10
     sleep_warning = 1
     vocab = [""]
@@ -27,29 +30,38 @@ class Pet(object):
     def __init__(self, name, pet_type):
         self.age = self.age
         self.excitement = randrange(self.excitement_max)
-        self.food = randrange(self.food_max)
+        self.food = randrange(self.food_min, self.food_max)
         self.level = self.level
         self.name = name
         self.pet_type = pet_type
         self.sleep = randrange(self.sleep_max)
         self.vocab = self.vocab[:]
 
-    # to start your timer
+     # Function created to reduce variables
+    def __clock_tick(self):
+        self.food -= 1
+        if self.food <= self.food_min:
+            self.food = self.food_min
+        
+        self.sleep -= 1
+        if self.sleep <= self.sleep_min:
+            self.sleep = self.sleep_min
+        
+        self.excitement -=1
+        if self.excitement <= self.excitement_min:
+            self.excitement = self.excitement_min
+        
+        self.age += 2
+        self.make_alive()
+    
+    # to start timer
     def make_alive(self):
         self.timer_task = threading.Timer(PERIOD_SEC, self.__clock_tick)
         self.timer_task.start()
 
-    # to stop your timer
+    # to stop timer
     def kill(self):
         self.timer_task.cancel()
-
-     # Function created to reduce variables
-    def __clock_tick(self):
-        self.food -= 1
-        self.sleep -= 1
-        self.excitement -=1
-        self.age += 2
-        
     
     # Set the status of the pets mood
     def mood(self):
@@ -83,40 +95,38 @@ class Pet(object):
         print(self.status())
         meal = randrange(1,5)
         self.food += meal
-
-        if self.food < 0:
-            self.food = 0
-            print ("I'm still ", self.mood)
-        else:
-            self.food > self.food_max
+        if self.food > self.food_max:
             self.food = self.food_max
-            print("***BURP*** I'm so full")
-    
+
+        self.sleep -= self.sleep_reduce
+
+        
     # Play with pet and increase excitement
     # Print the status of the pet
     def play(self):
-        print("WOOHOO!")
+        print('WOOHOOO!')
         print(self.status())
         fun = randrange(1,5)
         self.excitement += fun
-        self.sleep -= self.sleep_reduce
-        self.food -= self.sleep_reduce
-
-        if self.excitement < 0:
-            self.excitement = 0
-            print ("I'm still ", self.mood())
-        else: 
-            self.excitement > self.excitement_max
+        if self.excitement > self.excitement_max:
             self.excitement = self.excitement_max
-            print("I'm having SO MUCH FUN!")
+
+        self.sleep -= self.sleep_reduce
+        self.food -= self.food_reduce
+
+    def bedtime(self):
+        print('***YAWN***')
+        time.sleep(5)
+        print('zzz...')
+        time.sleep(5)
 
     # Print status of pets food, sleep and excitement
     def status(self):
-        print("I\'m ", self.mood())
         print("Hunger level", self.food)
         print("Sleep level ", self.sleep)
         print("Excitement level " , self.excitement)
         print("Age is " , self.age) 
+        print("I\'m ", self.mood())
 
 # Created main for user to create pet and print pet details
 def main():
@@ -126,6 +136,7 @@ def main():
     # Create new pet
     my_pet = Pet(pet_name, pet_type)
     my_pet.make_alive()
+
 
     def print_menu():
         print("1 - Feed", my_pet.name)
@@ -147,13 +158,19 @@ def main():
         elif option == 3:
             my_pet.teach()
         elif option == 4:
-            print("In progress..")
+            my_pet.bedtime()
         elif option == 5:
             print("Quitting in progress..")
         else:
-            print("Bye")
-            break
-
-        print_menu()
+            print("Oops! That's an invalid option")
         
+        print_menu()
+
+        if my_pet.food <= my_pet.food_min:
+            my_pet.food = my_pet.food_min
+        if my_pet.sleep <= my_pet.sleep_min:
+            my_pet.sleep = my_pet.sleep_min
+        if my_pet.excitement <= my_pet.excitement_min:
+            my_pet.excitement = my_pet.excitement_min
+
 main()
